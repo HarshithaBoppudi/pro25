ed578e291059d75548eefe332fadff9550334caa
HarshithaBoppudi <67325628+HarshithaBoppudi@users.noreply.github.com>
2020-07-21 18:51:48 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,55 +1,63 @@
-var ground;
+ 
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-const Constraint=Matter.Constraint;
-function preload()
-{
-	
-}
+
+
 
 function setup() {
 	createCanvas(1300, 400);
+
+
 	engine = Engine.create();
 	world = engine.world;
+
+	/*rect1=createSprite(900,300,15,100);
+	rect1.shapeColor=color("red");
+	rect2=createSprite(977,360,160,15);
+	rect2.shapeColor=color("red");
+	rect3=createSprite(1060,300,15,100);
+	rect3.shapeColor=color("red");
+	ground=createSprite(650,380,1300,20);
+	ground.shapeColor=color("aqua");*/
 	var ground_options={
 		isStatic:true,
 
 	}
-	ground=Bodies.rectangle(650,390,1300,20,ground_options);
-	
+	ground=Bodies.rectangle(650,380,1300,20,ground_options);
 	World.add(world,ground);
-	bob1=new bob(550,180,20);
-	bob2=new bob(650,200,20);
-	bob3=new bob(700,240,20);
-	bob4=new bob(750,280,20);
-	bob5=new bob(600,300,20);
-	wall1=new wall(650,60,350,20);
-	rope1= new string(bob2.body,wall1.body,-40,0);
+	waste1=new waste(200,300,1);
+	dustbin1=new dustbin(900,270,15,150);
+	//dustbin2=new dustbin(1040,360,250,15);
+	//dustbin3=new dustbin(1180,300,15,150);
 	Engine.run(engine);
-  
 }
 
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
  
+  background(222);
+  rectMode(CENTER);
   Engine.update(engine);
-	bob1.display();
-	bob2.display();
-	bob3.display();
-	bob4.display();
-	bob5.display();
-	rect(ground.position.x,ground.position.y,1300,20);
-	rope1.display();
-	 wall1.display();
-	
+  waste1.display();
+  rect(ground.position.x,ground.position.y,1300,20);
 
+  dustbin1.display();
+  // dustbin2.display();
+  // dustbin3.display();
+  // keyPressed();
   drawSprites();
  
 }
+function keyPressed(){
 
+	if(keyCode===UP_ARROW){
+
+	Matter.Body.applyForce(waste1.body,waste1.body.position,{x:0.5,y:-0.3});
+
+	}
+}
 
 
